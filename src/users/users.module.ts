ca68f128035d8f15f921517c366b9ac264365d3b
sathish v <sathish.v@poorvika.com>
2026-02-22 17:41:12 +0530

    Add user entity and register it to the global and user module.\

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  controllers: [UsersController],
+  providers: [UsersService],
+})
 export class UsersModule {}
