1f9a303b1ba50ed4ccdfe69b071e0ec6ecd29c2d
sathish v <sathish.v@poorvika.com>
2026-02-22 20:45:05 +0530

    Wire up service, controller and repo together.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
     console.log('entered the route');
-    console.log(body);
+    this.usersService.create(body.email, body.password);
   }
 }
